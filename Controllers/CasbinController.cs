37afa26f77ca6d84b8f935a759f598a06b5c5167
Sagar Khandelwal <sagarkhandelwal187@gmail.com>
2022-01-31 13:26:48 +0530

    Initial Commit

---
+++ Controllers/CasbinController.cs
@@ -0,0 +1,164 @@
+﻿using CasbinRBAC.Persistance;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using NetCasbin;
+using System;
+using System.Linq;
+
+namespace CasbinRBAC.Controllers
+{
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CasbinController : Controller
+    {
+        #region Fields
+        private readonly IConfiguration _configuration;
+        private readonly Enforcer _enforcer;
+        private readonly ApplicationDBContext _applicationDBContext;
+        private readonly string unauthorised = "Unauthorised User";
+        #endregion
+
+        #region Ctor
+        public CasbinController(CasbinDbContext<int> databaseContext, IConfiguration configuration, ApplicationDBContext applicationDBContext)
+        {
+            _configuration = configuration;
+            var efAdapter = new CasbinDbAdapter<int>(databaseContext);
+            _enforcer = new Enforcer("CasbinConfig/rbac_model.conf", efAdapter);
+            _applicationDBContext = applicationDBContext;
+        }
+        #endregion
+
+        /// <summary>
+        /// sub is for user
+        /// dom is for aff
+        /// obj is for roleName
+        /// act is for privilege
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="aff"></param>
+        /// <param name="roleName"></param>
+        /// <param name="privilege"></param>
+        [HttpGet("checkRbacWithAffiliation")]
+        public IActionResult CheckRbacWithAffiliation(string user, string aff, string roleName, string privilege, string permission)
+        {
+            var response = _enforcer.Enforce(user, aff, roleName, privilege, permission);
+            return Ok(response);
+        }
+
+        [HttpGet("Read")]
+        public Object Read(string user, string aff, string roleName, string privilege, string permission)
+        {
+            bool response = _enforcer.Enforce(user, aff, roleName, privilege, permission);
+            if(response)
+            {
+                try
+                {
+
+                    var a = _applicationDBContext.Contacts.FromSqlRaw("EXECUTE AS USER = {0} SELECT * FROM dbo.Contacts REVERT; ", user).ToList();
+                    return a;
+                    
+                }
+                catch (Exception e)
+                {
+                    return e.Message;
+                }
+            }
+            else
+            {
+                return unauthorised;
+            }
+            
+        }
+
+        [HttpGet("Insert")]
+        public String Insert(string user, string aff, string roleName, string privilege, string permission)
+        {
+            bool response = _enforcer.Enforce(user, aff, roleName, privilege, permission);
+            if (response)
+            {
+                try
+                {
+                    string name = "User2", address = "AddUser1", city = "CityUser1", state = "StateUser1", zip = "ZipUser1", email = "User1@gmail.com";
+
+                    var commandText = "EXECUTE AS USER = @p0 Insert into dbo.Contacts(Name, Address, City, State, Zip, Email) Values(@p1, @p2, @p3, @p4, @p5, @p6) REVERT; ";
+
+                    var result = _applicationDBContext.Database.ExecuteSqlRaw(commandText, user, name, address, city, state, zip, email);
+
+                    return "Data Inserted";
+                }
+                catch (Exception e)
+                {
+                    return e.Message; 
+                }
+            }
+            else
+            {
+                return unauthorised;
+            }
+        }
+
+        [HttpGet("Update")]
+        public String Update(string user, string aff, string roleName, string privilege, string permission)
+        {
+            bool response = _enforcer.Enforce(user, aff, roleName, privilege, permission);
+            if (response)
+            {
+                try
+                {
+                    string zip = "ZipUser4";
+                    int contactId = 4;
+                    var commandText = "EXECUTE AS USER = @p0 UPDATE Contacts SET Zip = @p1 WHERE ContactId = @p2 REVERT";
+                    var result = _applicationDBContext.Database.ExecuteSqlRaw(commandText, user, zip, contactId);
+
+                    if(result == 0)
+                    {
+                        return "User don't have permission to update the data on a particular row or 0 affected rows";
+                    }
+                    return "Data Updated";
+                }
+                catch (Exception e)
+                {
+                    return e.Message;
+                }
+            }
+            else
+            {
+                return unauthorised;
+            }
+        }
+
+        [HttpGet("Delete")]
+        public String Delete(string user, string aff, string roleName, string privilege, string permission)
+        {
+            bool response = _enforcer.Enforce(user, aff, roleName, privilege, permission);
+            if (response)
+            {
+                try
+                {
+                    int contactId = 4;
+                    var commandText = "EXECUTE AS USER = @p0 DELETE FROM dbo.Contacts WHERE ContactId = @p1 REVERT";
+
+                    var result = _applicationDBContext.Database.ExecuteSqlRaw(commandText, user, contactId);
+
+                    if (result == 0)
+                    {
+                        return "User don't have permission to delete the data on a particular row or 0 affected rows";
+                    }
+
+                    return "Data Deleted";
+                }
+                catch (Exception e)
+                {
+                    return e.Message;
+                }
+            }
+            else
+            {
+                return unauthorised;
+            }
+        }
+    }
+    
+}
---
+++ Persistance/CasbinDbContext.cs
@@ -0,0 +1,64 @@
+﻿using CasbinRBAC.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CasbinRBAC.Persistance
+{
+    public class CasbinDbContext<TKey> : DbContext where TKey : IEquatable<TKey>
+    {
+        #region Properties
+        public virtual DbSet<CasbinRule<TKey>> CasbinRule { get; set; }
+        #endregion
+
+        #region Fields
+        private readonly IEntityTypeConfiguration<CasbinRule<TKey>> _casbinModelConfig;
+        private readonly string _defaultSchemaName;
+        #endregion
+
+        #region Ctors
+        public CasbinDbContext()
+        {
+        }
+
+        public CasbinDbContext(DbContextOptions<CasbinDbContext<TKey>> options, string defaultSchemaName = null, string defaultTableName = "casbin_rule") : base(options)
+        {
+            _casbinModelConfig = new DefaultCasbinRuleEntityTypeConfiguration<TKey>(defaultTableName);
+            _defaultSchemaName = defaultSchemaName;
+        }
+
+        public CasbinDbContext(DbContextOptions<CasbinDbContext<TKey>> options, IEntityTypeConfiguration<CasbinRule<TKey>> casbinModelConfig, string defaultSchemaName = null) : base(options)
+        {
+            _casbinModelConfig = casbinModelConfig;
+            _defaultSchemaName = defaultSchemaName;
+        }
+
+        protected CasbinDbContext(DbContextOptions options, string defaultSchemaName = null, string defaultTableName = "casbin_rule") : base(options)
+        {
+            _casbinModelConfig = new DefaultCasbinRuleEntityTypeConfiguration<TKey>(defaultTableName);
+            _defaultSchemaName = defaultSchemaName;
+        }
+
+        protected CasbinDbContext(DbContextOptions options, IEntityTypeConfiguration<CasbinRule<TKey>> casbinModelConfig, string defaultSchemaName = null) : base(options)
+        {
+            _casbinModelConfig = casbinModelConfig;
+            _defaultSchemaName = defaultSchemaName;
+        }
+        #endregion
+
+        #region Functions
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            if (string.IsNullOrWhiteSpace(_defaultSchemaName) is false)
+            {
+                modelBuilder.HasDefaultSchema(_defaultSchemaName);
+            }
+
+            if (_casbinModelConfig is not null)
+            {
+                modelBuilder.ApplyConfiguration(_casbinModelConfig);
+            }
+
+        }
+        #endregion
+    }
+}
